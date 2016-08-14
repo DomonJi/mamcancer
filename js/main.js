108c246c0c2c614dfab1534b61c02991d5ea4da5
domon <domonji95@gmail.com>
2016-08-14 15:06:10 +0800

    the framework

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+    var $audioPlayer;
+    var $videoPlayer;
+    var screenManager;
+    var nextAction;
+    var $stage = $('.stage');
+    var $player = $('.player');
+
+    var videoInfos={
+
+    };
+
+    var handler ={
+        onPlayerInited: function(){
+
+        },
+        onVideoEnd:function(){
+
+        },
+        onMakeChoice:function(e,target){
+
+        }
+    };
+});
