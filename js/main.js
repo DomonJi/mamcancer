5a3fc4c9f0bd66cc25c0d14f38c82ba88974bcbb
domon <domonji95@gmail.com>
2016-08-14 15:30:34 +0800

    8.14

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(function(){
-    var $audioPlayer;
-    var $videoPlayer;
+    var $audio=$('.audio');
+    var $video=$('.video');
     var screenManager;
     var nextAction;
     var $stage = $('.stage');
-    var $player = $('.player');
+    var $videoPlayer = $('.video_player');
 
     var videoInfos={
 
@@ -19,6 +19,30 @@ $(function(){
         },
         onMakeChoice:function(e,target){
 
+        },
+        onOrientationChange: function() {
+            var ori = window.orientation || 0;
+
+            if (ori == 180 || ori === 0) {
+                $stage.height(window.innerWidth).width(window.innerHeight);
+            } else if (ori == 90 || ori == -90) {
+                $stage.height(window.innerHeight).width(window.innerWidth);
+            }
+        },
+        onTap:function(e){
+
         }
     };
+
+    (function(){
+        $(document.body).on('tap',handler.onTap);
+        window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize",
+        handler.onOrientationChange, false);
+        handler.onorientationchange();
+        var ori = window.orientation || 0;
+        if (ori == 180 || ori === 0) {
+            $video.width = $stage.height();
+            $video.height = $stage.width();
+        }
+    }());
 });
