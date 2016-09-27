7af45e09477fd67a941dc6b72c128403a937f119
domon <domonji95@gmail.com>
2016-09-27 16:02:57 +0800

    ios10

--- build/bundle.js
+++ build/bundle.js
@@ -57,7 +57,7 @@
 	  document.body.style.display = 'block';
 	  var video = document.getElementById('video');
 	  var audio = document.getElementById('audio');
-	  // video.controls = false;
+	  video.controls = false;
 	  var w = width / 640.0;
 	  var h = height / 1045.0;
 	  setTimeout(function () {
