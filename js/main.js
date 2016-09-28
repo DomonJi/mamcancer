54738ac07cd5a50eb4215c3cd79fc6f517ce60f0
domon <domonji95@gmail.com>
2016-09-28 22:10:17 +0800

    mamcancer

--- js/main.js
+++ js/main.js
@@ -1,114 +1,55 @@
-$(function() {
-    var $audio = $('.audio');
-    var $video = $('.video');
-    var screenManager;
-    var nextAction;
-    var $stage = $('.stage');
-    var $videoPlayer = $('.video_player');
+var dpr = window.devicePixelRatio || 1;
+var width = document.documentElement.clientWidth * dpr;
+var height = document.documentElement.clientHeight * dpr;
+var $ = require('jquery')
+// require('./rem.js');
+require('../postcss/index.css');
 
-    var videoInfos = {
+window.onload = function() {
+  document.body.style.display = 'block';
+  var video = document.getElementById('video');
+  var audio = document.getElementById('audio');
+  var w = width / 640.0;
+  var h = height / 1045.0;
+  // video.play();
+  setTimeout(function() {
+    setInterval(function() {
+      if (audio.volume > 0.06)
+        audio.volume -= 0.02;
+      }
+    , 100);
+  }, 46000);
+  video.addEventListener('ended', function(e) {
+    setTimeout(function() {
+      window.location.href = "http://woman.disoul.me";
+      document.getElementById('btn').style.opacity = 1;
+    }, 750);
+  });
+  video.addEventListener('timeupdate', function(e) {
+    if (e.target.currentTime > 51) {
+      window.location.href = "http://woman.disoul.me";
+    }
+  });
+  scaleToFill(video);
+  video.play();
+  audio.play();
+}
 
-    };
+function check() {
+  if (video.currentTime >= 52) {
+    window.location.href = "http://woman.disoul.me";
+  }
+  requestAnimationFrame(check);
+}
 
-    var showScreen = function() {
-        var that = this;
-        this.$el.show();
-    };
-
-    var hideScreen = function() {
-        var that = this;
-        setTimeout(function() {
-
-        }, 1000);
-    };
-
-    var screens = [{
-        $el: null,
-        index: 0,
-        show: function() {
-
-        },
-        hide: function() {
-
-        }
-    }, {
-        $el: null,
-        index: 1,
-        show: showScreen,
-        hide: hideScreen
-    }, {
-        $el: null,
-        index: 2,
-        show: showScreen,
-        hide: hideScreen
-    }, {
-        $el: null,
-        index: 3,
-        show: showScreen,
-        hide: hideScreen
-    }, {
-        $el: null,
-        index: 4,
-        show: showScreen,
-        hide: hideScreen
-    }];
-
-    screenManager = {
-        init: function() {
-
-        },
-        next: function() {
-
-        },
-        nextVideo: function() {
-
-        },
-        replay: function() {
-
-        },
-        rechoose: function() {
-
-        },
-        hide: function() {
-
-        }
-
-    };
-
-    var handler = {
-        onPlayerInited: function() {
-
-        },
-        onVideoEnd: function() {
-
-        },
-        onMakeChoice: function(e, target) {
-
-        },
-        onOrientationChange: function() {
-            var ori = window.orientation || 0;
-
-            if (ori == 180 || ori === 0) {
-                $stage.height(window.innerWidth).width(window.innerHeight);
-            } else if (ori == 90 || ori == -90) {
-                $stage.height(window.innerHeight).width(window.innerWidth);
-            }
-        },
-        onTap: function(e) {
-
-        }
-    };
-
-    (function() {
-        $(document.body).on('tap', handler.onTap);
-        window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize",
-            handler.onOrientationChange, false);
-        handler.onorientationchange();
-        var ori = window.orientation || 0;
-        if (ori == 180 || ori === 0) {
-            $video.width = $stage.height();
-            $video.height = $stage.width();
-        }
-        $('.video').on('ended', handler.onVideoEnd);
-    }());
-});
+function scaleToFill(videoTag) {
+  var $video = $(videoTag),
+    videoRatio = videoTag.videoWidth / videoTag.videoHeight,
+    tagRatio = $video.width() / $video.height();
+  if (videoRatio < tagRatio) {
+    $video.css('-webkit-transform', 'scaleX(' + tagRatio / videoRatio + ')')
+  } else if (tagRatio < videoRatio) {
+    $video.css('-webkit-transform', 'scaleY(' + videoRatio / tagRatio + ')')
+  }
+}
+requestAnimationFrame(check);
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var precss       = require('precss');
+var autoprefixer = require('autoprefixer');
+var pxtorem = require('postcss-pxtorem');
+
+module.exports = {
+  entry: "./js/main.js",
+  output: {
+    path: __dirname,
+    filename: "build/bundle.js",
+  },
+  module: {
+    loaders: [
+      { test: /\.(jpg|png|woff|woff2|eot|ttf|svg|opus)$/, loader: 'url-loader?limit=100000' },
+      { test: /\.css$/, loader: "style-loader!css-loader!postcss-loader"},
+      { test: /\.vue$/, loader: "vue" },
+      { test: /.js$/, loader: "babel" , exclude: /node_modules/ }
+    ]
+  },
+  postcss: function(){
+    return [precss, autoprefixer];
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  }
+};
