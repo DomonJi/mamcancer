32375bfa2870766777a6bfaf555dd87a1faf2db3
domon <domonji95@gmail.com>
2016-09-30 20:12:21 +0800

    completed

--- js/main.js
+++ js/main.js
@@ -4,45 +4,65 @@ var height = document.documentElement.clientHeight * dpr;
 var $ = require('jquery')
 // require('./rem.js');
 require('../postcss/index.css');
-
+let $videoa = $('#videoa')
+var ua = navigator.userAgent.toLowerCase();
+if (/iphone|ipad|ipod/.test(ua)) {
+  var $vide = $('#video')
+  var video = document.getElementById('video');
+  $videoa.remove()
+} else {
+  $('#video').remove()
+  var $video = $('#videoa')
+  var video = document.getElementById('videoa');
+}
 window.onload = function() {
   document.body.style.display = 'block';
-  var video = document.getElementById('video');
   var audio = document.getElementById('audio');
-  let $video = $('#video')
+  // let $video = $('#video')
   var w = width / 640.0;
   var h = height / 1045.0;
   let $img = $('.img')
+  // if (/iphone|ipad|ipod/.test(ua)) {
+  //   $video.attr('src', '../assets/source3.mp4');
+  //   audio.play()
+  //   video.play()
+  // } else {}
   // video.play();
-  setTimeout(function() {
-    setInterval(function() {
-      if (audio.volume > 0.06)
-        audio.volume -= 0.02;
-      }
-    , 100);
-  }, 46000);
+  // setTimeout(function() {
+  //     setInterval(function() {
+  //         if (audio.volume > 0.06)
+  //             audio.volume -= 0.02;
+  //     }, 100);
+  // }, 46000);
   video.addEventListener('ended', function(e) {
     setTimeout(function() {
       $img.show()
-      $video.hidden()
+      $video.remove()
+      if (/iphone|ipad|ipod/.test(ua)) {} else {
+        audio.play();
+      }
       // window.location.href = "http://woman.disoul.me";
-      document.getElementById('btn').style.opacity = 1;
-    }, 750);
+      // document.getElementById('btn').style.opacity = 1;
+    }, 400);
   });
   video.addEventListener('timeupdate', function(e) {
-    if (e.target.currentTime > 51) {
+    if (e.target.currentTime > 40) {
       // window.location.href = "http://woman.disoul.me";
       $img.show()
-      $video.hidden()
+      $video.remove()
     }
   });
-  scaleToFill(video);
+  if (/iphone|ipad|ipod/.test(ua)) {
+    audio.play();
+  }
   video.play();
-  audio.play();
+  setTimeout(function() {
+    scaleToFill(video);
+  }, 10)
 }
 
 function check() {
-  if (video.currentTime >= 52) {
+  if ($video.currentTime >= 42) {
     // window.location.href = "http://woman.disoul.me";
   }
   requestAnimationFrame(check);
---
+++ query/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="app_wrapper">
+<div class="logo">
+  <div class="logo__main"><img src="./assets/logo_main.png" /></div>
+  <div class="logo__logos"><img src="./assets/logos.png" /></div>
+  <div class="logo__music"><img src="./assets/music.png" /></div>
+</div>
+<div class="main">
+</div>
+</div>
+</template>
