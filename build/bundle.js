7bcba37b5c6235295b482eb2f1e2fb1372ee0560
domon <domonji95@gmail.com>
2016-09-30 15:07:12 +0800

    query

--- build/bundle.js
+++ build/bundle.js
@@ -54,70 +54,72 @@
 	__webpack_require__(2);
 	var $video = $('#video');
 	var ua = navigator.userAgent.toLowerCase();
-	if (/iphone|ipad|ipod/.test(ua)) {
-	    $video.attr('src', '../assets/source3.mp4');
-	} else {}
+	if (/iphone|ipad|ipod/.test(ua)) {} else {
+	  $video.attr('src', '../assets/source4.mp4');
+	}
 	window.onload = function () {
-	    document.body.style.display = 'block';
-	    var video = document.getElementById('video');
-	    var audio = document.getElementById('audio');
-	    var $video = $('#video');
-	    var w = width / 640.0;
-	    var h = height / 1045.0;
-	    var $img = $('.img');
-	    if (/iphone|ipad|ipod/.test(ua)) {
-	        $video.attr('src', '../assets/source3.mp4');
+	  document.body.style.display = 'block';
+	  var video = document.getElementById('video');
+	  var audio = document.getElementById('audio');
+	  var $video = $('#video');
+	  var w = width / 640.0;
+	  var h = height / 1045.0;
+	  var $img = $('.img');
+	  // if (/iphone|ipad|ipod/.test(ua)) {
+	  //   $video.attr('src', '../assets/source3.mp4');
+	  //   audio.play()
+	  //   video.play()
+	  // } else {}
+	  // video.play();
+	  // setTimeout(function() {
+	  //     setInterval(function() {
+	  //         if (audio.volume > 0.06)
+	  //             audio.volume -= 0.02;
+	  //     }, 100);
+	  // }, 46000);
+	  video.addEventListener('ended', function (e) {
+	    setTimeout(function () {
+	      $img.show();
+	      $video.remove();
+	      if (/iphone|ipad|ipod/.test(ua)) {} else {
 	        audio.play();
-	        video.play();
-	    } else {}
-	    // video.play();
-	    // setTimeout(function() {
-	    //     setInterval(function() {
-	    //         if (audio.volume > 0.06)
-	    //             audio.volume -= 0.02;
-	    //     }, 100);
-	    // }, 46000);
-	    video.addEventListener('ended', function (e) {
-	        setTimeout(function () {
-	            $img.show();
-	            $video.remove();
-	            if (/iphone|ipad|ipod/.test(ua)) {} else {
-	                audio.play();
-	            }
-	            // window.location.href = "http://woman.disoul.me";
-	            document.getElementById('btn').style.opacity = 1;
-	        }, 750);
-	    });
-	    video.addEventListener('timeupdate', function (e) {
-	        if (e.target.currentTime > 51) {
-	            // window.location.href = "http://woman.disoul.me";
-	            $img.show();
-	            $video.remove();
-	        }
-	    });
-	    scaleToFill(video);
-	    if (/iphone|ipad|ipod/.test(ua)) {
-	        audio.play();
+	      }
+	      // window.location.href = "http://woman.disoul.me";
+	      // document.getElementById('btn').style.opacity = 1;
+	    }, 400);
+	  });
+	  video.addEventListener('timeupdate', function (e) {
+	    if (e.target.currentTime > 40) {
+	      // window.location.href = "http://woman.disoul.me";
+	      $img.show();
+	      $video.remove();
 	    }
-	    video.play();
+	  });
+	  if (/iphone|ipad|ipod/.test(ua)) {
+	    audio.play();
+	  }
+	  video.play();
+	  setTimeout(function () {
+	    scaleToFill(video);
+	  }, 10);
 	};
 
 	function check() {
-	    if (video.currentTime >= 52) {
-	        // window.location.href = "http://woman.disoul.me";
-	    }
-	    requestAnimationFrame(check);
+	  if (video.currentTime >= 42) {
+	    // window.location.href = "http://woman.disoul.me";
+	  }
+	  requestAnimationFrame(check);
 	}
 
 	function scaleToFill(videoTag) {
-	    var $video = $(videoTag),
-	        videoRatio = videoTag.videoWidth / videoTag.videoHeight,
-	        tagRatio = $video.width() / $video.height();
-	    if (videoRatio < tagRatio) {
-	        $video.css('-webkit-transform', 'scaleX(' + tagRatio / videoRatio + ')');
-	    } else if (tagRatio < videoRatio) {
-	        $video.css('-webkit-transform', 'scaleY(' + videoRatio / tagRatio + ')');
-	    }
+	  var $video = $(videoTag),
+	      videoRatio = videoTag.videoWidth / videoTag.videoHeight,
+	      tagRatio = $video.width() / $video.height();
+	  if (videoRatio < tagRatio) {
+	    $video.css('-webkit-transform', 'scaleX(' + tagRatio / videoRatio + ')');
+	  } else if (tagRatio < videoRatio) {
+	    $video.css('-webkit-transform', 'scaleY(' + videoRatio / tagRatio + ')');
+	  }
 	}
 	requestAnimationFrame(check);
 
